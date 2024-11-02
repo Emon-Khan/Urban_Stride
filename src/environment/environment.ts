5b3ac3c66b95c8252422b8b5647c24ae64b95272
Emon-Khan <rkemon94@gmail.com>
2024-11-02 15:46:07 +0600

    Some random files have modified

---
+++ src/environment/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  apiUrl: 'https://b78499d2-00ea-45ad-8a8d-ab3ec2d39da2.mock.pstmn.io',
+  pageSize: 6,
+};
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,20 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
+import { provideAnimations } from '@angular/platform-browser/animations';
 import { AppComponent } from './app/app.component';
+import { provideRouter } from '@angular/router';
+import { routes } from './app/app.routes';
+import { PaginationModule } from 'ngx-bootstrap/pagination';
+import {
+  provideHttpClient,
+  withInterceptorsFromDi,
+} from '@angular/common/http';
+import { importProvidersFrom } from '@angular/core';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [
+    importProvidersFrom(PaginationModule.forRoot()),
+    provideAnimations(),
+    provideHttpClient(withInterceptorsFromDi()),
+    provideRouter(routes),
+  ],
+}).catch((err) => console.error(err));
